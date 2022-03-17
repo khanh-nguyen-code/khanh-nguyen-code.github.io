9e5a805765ba6c48e2b85fbdfc3dbe1538e653cd
Khanh Nguyen <khanh.nguyen.contact@gmail.com>
2022-03-17 13:35:14 +0800

    modified:   assets/cv_acad.pdf 	modified:   fileserver.go

--- fileserver.go
+++ fileserver.go
@@ -6,6 +6,6 @@ import "log"
 func main() {
         fs := http.FileServer(http.Dir("./"))
         http.Handle("/", http.StripPrefix("/", fs))
-        log.Println("Server is up: http://localhost:3000")
-        log.Fatal(http.ListenAndServe(":3000", nil))
+        log.Println("Server is up: http://0.0.0.0:3000")
+        log.Fatal(http.ListenAndServe("0.0.0.0:3000", nil))
 }
