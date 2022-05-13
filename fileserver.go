91e3ff8a0f43566fc74a45b1d8c8ab0957c0c77d
Khanh Nguyen <khanh.nguyen.contact@gmail.com>
2022-05-13 11:25:24 +0800

    commit

--- fileserver.go
+++ fileserver.go
@@ -1,11 +1,13 @@
 package main
 
-import "net/http"
-import "log"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-        fs := http.FileServer(http.Dir("./"))
-        http.Handle("/", http.StripPrefix("/", fs))
-        log.Println("Server is up: http://0.0.0.0:3000")
-        log.Fatal(http.ListenAndServe("0.0.0.0:3000", nil))
+	fs := http.FileServer(http.Dir("./"))
+	http.Handle("/", http.StripPrefix("/", fs))
+	log.Println("Server is up: http://127.0.0.1:3000")
+	log.Fatal(http.ListenAndServe("127.0.0.1:3000", nil))
 }
